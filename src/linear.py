e6462e37931963b5d353fec8842c282cafade760
Andrew Mark Elgert <andrew.elgert@gmail.com>
2016-11-04 01:09:42 -0400

    Lesson 3 code

--- src/linear.py
+++ src/linear.py
@@ -1,7 +1,14 @@
-import pandas as pd
 import quandl
+import math
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get("WIKI/GOOGL")
+
+print(df.head())
+#print(df.tail())
 
 df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']]
 
@@ -9,5 +16,10 @@ df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
-
 print(df.head())
+
+forecast_col = 'Adj. Close'
+df.fillna(value=-99999, inplace=True)
+forecast_out = int(math.ceil(0.01 * len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
